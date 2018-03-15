ff436d43a2c344c872158a12d69e60623b80f8b3
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-03-15 12:33:49 +0100

    Created the auth feature (and auth routing) module

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -2,28 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      HeaderComponent,
-      SignupComponent,
-      SigninComponent
+      HeaderComponent
    ],
    imports: [
       BrowserModule,
@@ -31,7 +27,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
       AppRoutingModule,
       RecipesModule,
       SharedModule,
-      ShoppingListModule
+      ShoppingListModule,
+      AuthModule
    ],
    providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
    bootstrap: [AppComponent]
