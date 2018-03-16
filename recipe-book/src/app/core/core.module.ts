9521bc6f27b5cbcd02150bb8760727f053896c4f
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-03-16 20:44:21 +0100

    Moltiple interceptors

--- recipe-book/src/app/core/core.module.ts
+++ recipe-book/src/app/core/core.module.ts
@@ -10,6 +10,7 @@ import { RecipeService } from '../recipes/recipe.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
 
 @NgModule({
    declarations: [
@@ -32,6 +33,11 @@ import { AuthInterceptor } from '../shared/auth.interceptor';
       {
          provide: HTTP_INTERCEPTORS,
          useClass: AuthInterceptor,
+         multi: true
+      },
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: LoggingInterceptor,
          multi: true
       }
    ]
