4b5d3c17673170970e060924c5bf1a84fb7175be
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-04-04 14:19:28 +0200

    More actions and adding ingredients

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
 
 @Component({
    selector: 'app-recipe-detail',
@@ -13,7 +16,7 @@ export class RecipeDetailComponent implements OnInit {
    recipe: Recipe;
    id: number;
 
-   constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
+   constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
    ngOnInit() {
       this.route.params
@@ -26,7 +29,7 @@ export class RecipeDetailComponent implements OnInit {
    }
 
    onAddToShoppingList() {
-      this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+      this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
    }
 
    onEditRecipe() {
