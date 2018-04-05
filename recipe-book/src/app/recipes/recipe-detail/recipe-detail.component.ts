e5f1ca12805e2defdd80c911f8241eec0c604d80
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-04-05 13:02:26 +0200

    Setting up the auth store files

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
-import * as fromShoppingList from '../../shopping-list/store/shopping-list.reducers';
+import * as fromApp from '../../store/app.reducers';
 
 @Component({
    selector: 'app-recipe-detail',
@@ -16,7 +16,7 @@ export class RecipeDetailComponent implements OnInit {
    recipe: Recipe;
    id: number;
 
-   constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router, private store: Store<fromShoppingList.AppState>) { }
+   constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router, private store: Store<fromApp.AppState>) { }
 
    ngOnInit() {
       this.route.params
