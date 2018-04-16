9f5caaf25c4229e5a1ab62ee0aa17de9853468f9
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-04-16 17:41:16 +0200

    Redirecting upon logout

--- recipe-book/src/app/auth/store/auth.effects.ts
+++ recipe-book/src/app/auth/store/auth.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import { fromPromise } from 'rxjs/observable/fromPromise';
@@ -64,6 +65,13 @@ export class AuthEffects {
                payload: token
             }
          ];
+      });
+
+   @Effect({ dispatch: false })
+   authLogout = this.actions$
+      .ofType(AuthActions.LOGOUT)
+      .do(() => {
+         this.router.navigate(['/']);
       });
 
    constructor(private actions$: Actions, private router: Router) { }
