5d507ac5df4f771551d06b1ad53499cb2c501749
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-04-16 15:14:27 +0200

    Auth effects and actions

---
+++ recipe-book/src/app/auth/store/auth.effects.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Effect, Actions } from '@ngrx/effects';
+
+@Injectable()
+export class AuthEffects {
+   @Effect()
+   authSignup;
+
+   constructor(private actions$: Actions) { }
+}
+
